833f862ab7eaa60c99b61dd857b4d48a96978ba6
Srujansujji1717 <puttasrujan20bsc74@iiitkottayam.ac.in>
2022-03-18 23:02:59 +0530

    binary search on reversed list

---
+++ Desending_BS.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define f(a) for (int i = 0; i <a;i++) 
+
+
+int main(){
+    int a,k;
+    cin >> a >>k;
+    int b[a];
+    f(a)
+    cin >> b[i];
+
+    int st=0;
+    int end=a-1;
+
+    while(st<=end){
+        int mid=st+(end-st)/2;
+        if(b[mid]==k){
+            cout<<mid<<endl;
+            return 0;
+
+        }
+        
+
+        else if(b[mid]>k){
+            st=mid+1;
+        }
+        else{
+            end=mid-1;
+        }
+    }
+    cout<<"not found"<<endl;
+}
